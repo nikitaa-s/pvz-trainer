91e7a4276ff00dc6fe334d42fddafb12234e9616
nstkachenko <nstkachenko@avito.ru>
2026-01-20 18:36:30 +0300

    init project

---
+++ PvzTrainer/PvzTrainer/AppDelegate.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // Экран, на который будут накладываться другие UIView элементы
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        // Для назначения этого window главным
+        window?.makeKeyAndVisible()
+        // Установка главного контроллера, с которого начнется работа приложения
+        window?.rootViewController = ViewController()
+        return true
+    }
+}
